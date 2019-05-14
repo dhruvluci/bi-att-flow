7187329b6be44d334fc42fcf870d3db420507dac
dhruvluci <39732134+dhruvluci@users.noreply.github.com>
2019-05-14 11:55:02 +0530

    except loop for qas

--- squad/evaluate-v1.1.py
+++ squad/evaluate-v1.1.py
@@ -55,20 +55,22 @@ def evaluate(dataset, predictions):
     f1 = exact_match = total = 0
     for article in dataset:
         for paragraph in article['paragraphs']:
-            for qa in paragraph['qas']:
-                total += 1
-                if qa['id'] not in predictions:
-                    message = 'Unanswered question ' + qa['id'] + \
-                              ' will receive score 0.'
-                    print(message, file=sys.stderr)
-                    continue
-                ground_truths = list(map(lambda x: x['text'], qa['answers']))
-                prediction = predictions[qa['id']]
-                exact_match += metric_max_over_ground_truths(
-                    exact_match_score, prediction, ground_truths)
-                f1 += metric_max_over_ground_truths(
-                    f1_score, prediction, ground_truths)
-
+            try:
+                for qa in paragraph['qas']:
+                    total += 1
+                    if qa['id'] not in predictions:
+                        message = 'Unanswered question ' + qa['id'] + \
+                                  ' will receive score 0.'
+                        print(message, file=sys.stderr)
+                        continue
+                    ground_truths = list(map(lambda x: x['text'], qa['answers']))
+                    prediction = predictions[qa['id']]
+                    exact_match += metric_max_over_ground_truths(
+                        exact_match_score, prediction, ground_truths)
+                    f1 += metric_max_over_ground_truths(
+                        f1_score, prediction, ground_truths)
+            except:
+                continue
     exact_match = 100.0 * exact_match / total
     f1 = 100.0 * f1 / total
 
